72d053af7af329687266139d7cd3212f8e9d71d3
Yeison Calle <yeiksite2@gmail.com>
2022-07-25 15:08:45 -0500

    lazy query

--- src/hooks/useAllPersons.js
+++ src/hooks/useAllPersons.js
@@ -1,8 +1,15 @@
 import useSWR from 'swr'
 import { ALL_PERSONS_QUERY } from '../apollo/graphql/queries/person-queries'
+import { useLazySWR } from './useLazySwr'
 
 export const useAllPersons = () => {
   const { data, error } = useSWR(ALL_PERSONS_QUERY)
 
   return { allPersons: data.allPersons, error }
+}
+
+export const useLazyAllPersons = () => {
+  const [executeAllPersons, response] = useLazySWR(ALL_PERSONS_QUERY)
+
+  return { executeAllPersons, response }
 }
